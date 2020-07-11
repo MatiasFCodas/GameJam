42287630dd3dfc0af9876ec0600ad57e0a063caf
Matias <matiascodas@gmail.com>
2020-07-10 21:59:00 -0300

    arrumando gitignore

---
+++ GameJam2020/Assets/Scripts/GameController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene("Teste");
+        }
+    }
+}
